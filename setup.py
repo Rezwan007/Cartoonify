6ea3b8e4d41d24281d50669008da35e4b9187ddf
Rezwan <utsha1234@gmail.com>
2021-04-21 13:39:50 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pythonProject',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Rezwan',
+    author_email='utsha1234@gmail.com',
+    description='Do you miss your childhood? Yes, everyone does.! So today let’s head towards giving our pictures some cartoonic effects. This article is all about building a photo cartoonifyer using Python and OpenCV'
+)
